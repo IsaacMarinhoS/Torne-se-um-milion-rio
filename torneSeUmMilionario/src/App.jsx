d972be6158bbf1dfbd60a868636308d8c03d4e9b
PedroDuvanel <pedrodbarboza@outlook.com>
2024-10-03 20:22:47 -0300

    Alteraçoes

--- torneSeUmMilionario/src/App.jsx
+++ torneSeUmMilionario/src/App.jsx
@@ -1,5 +1,9 @@
 import './App.css'
+<<<<<<< HEAD
 import { Header, header } from './components/header'
+=======
+import { Header } from './components/header'
+>>>>>>> c530722e9bb1a34997f3c4243e34a56337b8ef60
 
 
 function App() {
@@ -8,7 +12,11 @@ function App() {
   return (
     <>
     <Header/>
+<<<<<<< HEAD
 
+=======
+   
+>>>>>>> c530722e9bb1a34997f3c4243e34a56337b8ef60
     </>
   )
 }
